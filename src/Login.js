e3dc65813117c5a27f14174bf856ae9fb002f572
npx <nithinpaidi@lyrostech.com>
2024-03-11 17:58:39 +0530

    updated content

--- src/Login.js
+++ src/Login.js
@@ -1,23 +1,18 @@
 import React, { useContext } from 'react'
 import { UserContext } from './App'
-import Home from './Home';
 import Register from './Register';
 
 
 export default function Login() {
     const {flag, setFlag}=useContext(UserContext);
-if (flag == 1) {
-    <Home/>
-} else if (flag == 2) {
-    <Register/>
-} 
   return (
     <>
     <div>Login</div>
-    <button onClick={()=>setFlag((prev)=>1)}>Login</button>
-    <button onClick={()=>setFlag((prev)=>2)}>Create Account</button>
-    {console.log(flag)}
-     {flag && (<Home/>)}
+    <input type='text'></input><br></br>
+    <input type='text'></input><br></br>
+    <button onClick={()=>setFlag((prev)=>2)}>Login</button>
+    <button onClick={()=>setFlag((prev)=>1)}>Create Account</button>
+    {flag===1 && (<Register />)}
     </> 
   )
 }
